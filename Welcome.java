eb00a4218e4eaceaf2ac3b9d8ae19e27d332fa4b
Nguyen Van Binh <23639111.binh@student.iuh.edu.vn>
2024-08-13 22:01:51 +0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("\n Hello Nguyen Van Binh!");
+        System.out.println("Welcome to LTHDT_DHKTPM19A");
+    }
+}
